9fbb71bd96601b0d9bccfb9629d4372fccb3e2fc
Van Nguyen <vannguyen96>
2017-11-02 00:10:31 +0700

    server-setup

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database')
+const path = require('path');
+
+mongoose.Promise= global.Promise;
+mongoose.connect(config.uri,(err) =>{
+    if(err){
+        console.log('could not connect ',err);
+    }else{
+        console.log('Connected to data :'+config.db);
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist'));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+})
+
+app.listen(8080);
